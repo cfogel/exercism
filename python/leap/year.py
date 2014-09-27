d67edc3a80cc363c882f24eb698880e44155db3b
CJ Fogel <fogelcj@gmail.com>
2014-09-27 08:57:49 -0400

    Changed leap to use single if-else

--- python/leap/year.py
+++ python/leap/year.py
@@ -1,7 +1,5 @@
-def is_leap_year(self):
-	if self % 4 == 0:
-		if self % 100 == 0:
-			if self % 400 == 0:
-				return True
-			return False
-		return True
+def is_leap_year(yr):
+    if yr % 400 == 0 or (yr % 4 == 0 and yr % 100 != 0):
+        return True
+    else:
+        return False

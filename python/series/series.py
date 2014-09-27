e440788cf927110fee89b4a83d9908eeb19e5d67
CJ Fogel <fogelcj@gmail.com>
2014-09-27 09:04:48 -0400

    Converted series to ints at beginning

--- python/series/series.py
+++ python/series/series.py
@@ -1,7 +1,8 @@
 def slices(series, length):
     if length > len(series) or length < 1:
         raise ValueError
+    intlist = [int(a) for a in series]
     slicelist = []
-    for a in range(len(series)-length+1):
-        slicelist.append(list(series[a:a+length]))
-    return [[int(b) for b in c] for c in slicelist]
+    for a in range(len(intlist)-length+1):
+        slicelist.append(intlist[a:a+length])
+    return slicelist

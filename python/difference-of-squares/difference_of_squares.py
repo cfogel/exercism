fcb7e6ed8b521f84706c3ac1f1b41ce2b4c1760d
CJ Fogel <fogelcj@gmail.com>
2014-09-27 08:44:26 -0400

    Updated difference-of-squares to use sum

--- python/difference-of-squares/difference_of_squares.py
+++ python/difference-of-squares/difference_of_squares.py
@@ -1,12 +1,6 @@
-def square_of_sum(self):
-    count = 0
-    for a in range(self+1):
-        count += a
-    return count * count
-def sum_of_squares(self):
-    count = 0
-    for a in range(self+1):
-        count += a*a
-    return count
-def difference(self):
-    return abs(sum_of_squares(self) - square_of_sum(self))
+def square_of_sum(num):
+    return (sum(a for a in range(num+1))**2)
+def sum_of_squares(max):
+    return sum(b*b for b in range(max+1))
+def difference(diff):
+    return abs(sum_of_squares(diff) - square_of_sum(diff))
